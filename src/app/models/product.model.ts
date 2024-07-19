74ac09fea5839a5ca832280b6e727a36122190ac
NickolasKemp <kemplent@gmail.com>
2024-07-19 08:09:27 +0300

    feat: delivery options

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -4,15 +4,44 @@ export interface IProduct {
 	name: string;
 	description?: string;
 	price: number;
-	images: string[];
-	deliveryWay: EnumDeliveryWay;
-	deliveryPrice?: number;
-	deliveryPeriod?: string;
+	image: string;
+	deliveryOptions: [IDeliveryOption];
 	quantity: number;
+}
+
+export interface IDeliveryOption {
+	type: EnumDeliveryWay;
+	price: number;
+	period: string;
 }
 
 export enum EnumDeliveryWay {
 	COURIER = "COURIER",
 	POSTAL = "POSTAL",
 	PICKUP = "PICKUP",
+}
+
+export class Product implements Partial<IProduct> {
+	constructor(
+		public name = "",
+		public description = "",
+		public price = 0,
+		public image = "",
+		public deliveryOptions: [IDeliveryOption] = [new DeliveryOption()],
+		public quantity = 1,
+		public deliveryPrice = 0,
+		public deliveryPeriod = "",
+	) {}
+}
+
+export class DeliveryOption implements IDeliveryOption {
+	constructor(
+		public type = EnumDeliveryWay.PICKUP,
+		public period = "",
+		public price = 0,
+	) {
+		this.type = type;
+		this.period = period;
+		this.price = price;
+	}
 }
