4734c745541e39b118034c7d909cc5e1f4f7213c
NickolasKemp <kemplent@gmail.com>
2024-07-19 07:50:48 +0300

    feat: delete and update methods

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, map, Observable, switchMap, tap } from "rxjs";
-import { IProduct } from "../models/product.model";
+import { IDeliveryOption, IProduct } from "../models/product.model";
 import { environment } from "../../environments/environment.development";
 
 @Injectable({
@@ -32,9 +32,45 @@ export class ProductsService {
 			.pipe(tap(prod => this.productById$.next(prod)));
 	}
 
-	create(product: Omit<IProduct, "id">): Observable<IProduct> {
+	create(product: Omit<IProduct, "_id" | "createdAt">): Observable<IProduct> {
 		return this.http
 			.post<IProduct>(`${this.API_URL}/${this.BASE}`, product)
+			.pipe(switchMap(product => this.getAll().pipe(map(() => product))));
+	}
+
+	update(id: string, product: Partial<IProduct>): Observable<IProduct> {
+		return this.http
+			.put<IProduct>(`${this.API_URL}/${this.BASE}/${id}`, product)
+			.pipe(switchMap(product => this.getAll().pipe(map(() => product))));
+	}
+
+	addDeliveryOption(
+		id: string,
+		deliveryOption: IDeliveryOption,
+	): Observable<IProduct> {
+		return this.http
+			.patch<IProduct>(
+				`${this.API_URL}/${this.BASE}/${id}/delivery-options`,
+				deliveryOption,
+			)
+			.pipe(switchMap(product => this.getAll().pipe(map(() => product))));
+	}
+
+	deleteDeliveryOption(
+		productId: string,
+		deliveryOptionId: string,
+	): Observable<IProduct> {
+		return this.http
+			.patch<IProduct>(
+				`${this.API_URL}/${this.BASE}/${productId}/delivery-options/${deliveryOptionId}`,
+				{},
+			)
+			.pipe(switchMap(product => this.getAll().pipe(map(() => product))));
+	}
+
+	delete(id: string): Observable<IProduct> {
+		return this.http
+			.delete<IProduct>(`${this.API_URL}/${this.BASE}/${id}`)
 			.pipe(switchMap(product => this.getAll().pipe(map(() => product))));
 	}
 }
